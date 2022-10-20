028f2537184aa925bda84602af95496e739d584a
Abdullah Bashir <abdullahbr98@gmail.com>
2022-10-20 15:11:02 +0500

    doughnut Chart Added

--- client/src/pages/ManufacturerHomeScreen.js
+++ client/src/pages/ManufacturerHomeScreen.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import { Box, Flex, Text, Button, SimpleGrid } from "@chakra-ui/react";
 import ManufacturerTable from "../components/ManufacturerTable";
+import ManufacturerChart from "../components/ManufacturerChart";
 import ManufacturerSideBar from "../components/ManufacturerSideBar";
 import AuthenticSellersList from "../components/AuthenticSellersList";
 import { ArrowBackIcon } from "@chakra-ui/icons";
@@ -32,16 +33,32 @@ export default function ManufacturerHomeScreen() {
         <Box>
             <Box className="App">
                 <Box mx={100}>
-                <Navbar
-                    guestAccess={false}
-                    heading={"Manufacturer Dashboard"}
-                    manufacturerAccess={true}
-                />
+                    <Navbar
+                        guestAccess={false}
+                        heading={"Manufacturer Dashboard"}
+                        manufacturerAccess={true}
+                    />
                 </Box>
 
-                <Flex >
-                    <ManufacturerSideBar/>
+                <Flex>
+                    <ManufacturerSideBar />
                     <DashboardItems setProductsFunc={setProductsFunc} />
+                    <Box
+                        width="20vw"
+                        mt="4"
+                        h="50vh"
+                        pt="3"
+                        // pe="2"
+                        bg="white"
+                        // p={4}
+                        me={4}
+                        boxShadow="md"
+                        rounded="md"
+                        cursor="pointer"
+                        _hover={{ backgroundColor: "blackAlpha.50", boxShadow:"dark-lg" }}
+                    >
+                        <ManufacturerChart />
+                    </Box>
                 </Flex>
             </Box>
         </Box>
