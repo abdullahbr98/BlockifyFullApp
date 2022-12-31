939456cc050d81d1b52c8ecb4656b4a757dcc367
Abdullah Bashir <abdullahbr98@gmail.com>
2023-01-01 02:53:20 +0500

    front end changes

--- client/src/components/SellerProfileInfo.js
+++ client/src/components/SellerProfileInfo.js
@@ -1,16 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    useDisclosure,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
     Box,
     Text,
     Image,
+    useToast,
     Badge,
     Flex,
     Button,
@@ -27,38 +21,79 @@ import {
     TableContainer,
 } from "@chakra-ui/react";
 import { EditIcon } from "@chakra-ui/icons";
+
+//getOrderOfSeller
+
 export default function SellerProfileInfo({ profileInfoFunc }) {
-    const { isOpen, onOpen, onClose } = useDisclosure();
+    const toast = useToast();
+    const [orderArray, setorderArray] = useState([]);
+    const orderStatusHelperFunction = (status) => {
+        if (status == "placed") {
+            return "confirmed";
+        } else if (status == "confirmed") {
+            return "processed";
+        } else if (status == "processed") {
+            return "shipped";
+        }
+        return status;
+    };
+
+    const changeOrderStatusApi = async (id, status) => {
+        const data = await axios.post(
+            "http://localhost:8000/Order/changeOrderStatus",
+            {
+                orderId: id,
+                orderStatus: status,
+            }
+        );
+        toast({
+            title: "Order Status Updated.",
+            description: "We've Updated Order Status",
+            status: "success",
+            duration: 3000,
+            isClosable: true,
+        });
+    };
+
+    const changeSellerOrderStatus = (status) => {
+        if (status == "placed") {
+            return "confirm";
+        } else if (status == "confirmed") {
+            return "process";
+        } else if (status == "processed") {
+            return "ship";
+        }
+        return status;
+    };
+    const sellerOrdersRequest = async () => {
+        const items = JSON.parse(localStorage.getItem("UserAddress"));
+        const data = await axios.get(
+            "http://localhost:8000/Order/getOrderOfSeller",
+            {
+                params: {
+                    sellerAddress: items,
+                },
+            }
+        );
+        console.log();
+        console.log(data.data);
+        setorderArray(data.data);
+    };
+
+    useEffect(() => {
+        sellerOrdersRequest();
+    }, []);
+
     return (
         <>
             {profileInfoFunc ? (
                 <>
-                    <Modal isOpen={isOpen} onClose={onClose}>
-                        <ModalOverlay />
-                        <ModalContent>
-                            <ModalHeader>Order Status</ModalHeader>
-                            <ModalCloseButton />
-                            <ModalBody>
-                                <Text>Hello</Text>
-                            </ModalBody>
-
-                            <ModalFooter>
-                                <Button
-                                    colorScheme="red"
-                                    mr={3}
-                                    onClick={onClose}
-                                >
-                                    Close
-                                </Button>
-                                <Button variant="ghost">
-                                    Secondary Action
-                                </Button>
-                            </ModalFooter>
-                        </ModalContent>
-                    </Modal>
                     <Box align="center" w="90vw">
                         <Flex justifyContent="center">
                             <Text fontSize="2xl">Orders Details</Text>
+                        </Flex>
+                        <Flex justifyContent="center">
+                            <Text fontSize="lg" mt="4">Press the order status button below to updated<br/>the order status shown below</Text>
                         </Flex>
                         <Box mt="10vh" align="center" px={10}>
                             <TableContainer>
@@ -70,60 +105,81 @@ export default function SellerProfileInfo({ profileInfoFunc }) {
                                             <Th>Payment Method</Th>
                                             <Th>Payment Status</Th>
                                             <Th>Order Status</Th>
-                                            <Th>Edit Order Status</Th>
                                         </Tr>
                                     </Thead>
                                     <Tbody>
-                                        <Tr>
-                                            <Td>32423</Td>
-                                            <Td>500$</Td>
-                                            <Td>Stripe</Td>
-                                            <Td>
-                                                <Flex>
-                                                    <Badge
-                                                        colorScheme="green"
-                                                        borderRadius={4}
-                                                        borderColor="green.500"
-                                                        borderWidth="1px"
-                                                    >
-                                                        Paid
-                                                    </Badge>
-                                                    <Badge
-                                                        colorScheme="red"
-                                                        borderRadius={4}
-                                                        borderColor="red.500"
-                                                        borderWidth="1px"
-                                                    >
-                                                        Unpaid
-                                                    </Badge>
-                                                </Flex>
-                                            </Td>
-                                            <Td>
-                                                <Flex>
-                                                    <Badge
-                                                        colorScheme="green"
-                                                        borderRadius={4}
-                                                        borderColor="green.500"
-                                                        borderWidth="1px"
-                                                    >
-                                                        Shipped
-                                                    </Badge>
-                                                    <Badge
-                                                        colorScheme="yellow"
-                                                        borderRadius={4}
-                                                        borderColor="yellow.500"
-                                                        borderWidth="1px"
-                                                    >
-                                                        In transit
-                                                    </Badge>
-                                                </Flex>
-                                            </Td>
-                                            <Td>
-                                                <Link onClick={onOpen}>
-                                                    View Details{" "}
-                                                </Link>
-                                            </Td>
-                                        </Tr>
+                                        {orderArray?.map((orderArray) => {
+                                            console.log(orderArray);
+                                            return (
+                                                <Tr>
+                                                    <Td>
+                                                        {orderArray.orderId}
+                                                    </Td>
+                                                    <Td>
+                                                        {orderArray.orderAmount}
+                                                        $
+                                                    </Td>
+                                                    <Td>
+                                                        {
+                                                            orderArray.paymentMethod
+                                                        }
+                                                    </Td>
+                                                    <Td align="center">
+                                                        <Flex>
+                                                            <Text
+                                                                fontWeight="bold"
+                                                                color="green"
+                                                                align="center"
+                                                                ps="40px"
+                                                            >
+                                                                {
+                                                                    orderArray.paymentStatus
+                                                                }
+                                                            </Text>
+                                                        </Flex>
+                                                    </Td>
+                                                    <Td>
+                                                        <Flex>
+                                                            <Badge
+                                                                cursor="pointer"
+                                                                colorScheme="green"
+                                                                borderRadius={4}
+                                                                borderColor="green.500"
+                                                                borderWidth="1px"
+                                                                onClick={() => {
+                                                                    changeOrderStatusApi(
+                                                                        orderArray.orderId,
+                                                                        orderStatusHelperFunction(
+                                                                            orderArray.orderStatus
+                                                                        )
+                                                                    );
+                                                                }}
+                                                            >
+                                                                {changeSellerOrderStatus(
+                                                                    orderArray.orderStatus
+                                                                )}
+                                                            </Badge>
+                                                            {/* <Badge
+                                                                colorScheme="yellow"
+                                                                borderRadius={4}
+                                                                borderColor="yellow.500"
+                                                                borderWidth="1px"
+                                                            >
+                                                                Processed
+                                                            </Badge>
+                                                            <Badge
+                                                                colorScheme="purple"
+                                                                borderRadius={4}
+                                                                borderColor="purple.500"
+                                                                borderWidth="1px"
+                                                            >
+                                                                Confirm Shipment
+                                                            </Badge> */}
+                                                        </Flex>
+                                                    </Td>
+                                                </Tr>
+                                            );
+                                        })}
                                     </Tbody>
                                 </Table>
                             </TableContainer>
